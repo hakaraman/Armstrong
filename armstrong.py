3f3f8b7b2bc61bd482ba74e7a29e21ae979c64a8
Hakan <hakankaraman1@gmail.com>
2020-07-13 23:49:39 +0300

    Armstrong.py dosyası oluşturuldu ve içine kod yazıldı.

---
+++ armstrong.py
@@ -0,0 +1,35 @@
+num = (input("Lütfen bir sayý giriniz: "))
+
+
+length =(len(num))
+
+tup = (num,)
+
+amst = (0)
+
+ams = (0)
+
+
+if not num.isdigit() == True:
+
+    print(num, ": It is an invalid entry. Don't use non-numeric, float, or negative values!")
+
+
+elif (int(num) < 0):
+
+    print(num, ": It is an invalid entry. Don't use non-numeric, float, or negative values!")
+
+
+else:
+ 
+   for i in range(length):  #print(int(tup[0][i])**length)
+
+        amst += (ams + (int(tup[0][i])**length))
+
+    if amst == int(num):
+
+        print(amst, 'is an Armstrong number')
+
+    else:
+
+        print(amst, "is not an Armstrong number")
