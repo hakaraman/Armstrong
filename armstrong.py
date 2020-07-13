0eee49b82134107d66d543e9a586a728bdc88fdd
hakaraman <67653392+hakaraman@users.noreply.github.com>
2020-07-14 00:00:43 +0300

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,35 +1,16 @@
-num = (input("Lütfen bir sayı giriniz: "))
-
-
+num = (input("LÃ¼tfen bir sayÃ½ giriniz: "))
 length =(len(num))
-
 tup = (num,)
-
 amst = (0)
-
 ams = (0)
-
-
 if not num.isdigit() == True:
-
     print(num, ": It is an invalid entry. Don't use non-numeric, float, or negative values!")
-
-
 elif (int(num) < 0):
-
     print(num, ": It is an invalid entry. Don't use non-numeric, float, or negative values!")
-
-
 else:
- 
-   for i in range(length):  #print(int(tup[0][i])**length)
-
+    for i in range(length):  #print(int(tup[0][i])**length)
         amst += (ams + (int(tup[0][i])**length))
-
     if amst == int(num):
-
         print(amst, 'is an Armstrong number')
-
     else:
-
         print(amst, "is not an Armstrong number")
